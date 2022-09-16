95d1ca055ea60dcbb77c45f5e1953606e3e86acf
Diego Lima de Oliveira <diego.dromer@hotmail.com>
2022-09-15 23:02:36 -0300

    login de usuário, autorização de métodos para usuários autenticados e não autenticados implementados.

--- LanchesMac/Controllers/CarrinhoCompraController.cs
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -1,6 +1,7 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -29,6 +30,7 @@ namespace LanchesMac.Controllers
             return View(carrinhoCompraVM);
         }
 
+        [Authorize]
         public IActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
         {
             var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);  
@@ -39,6 +41,7 @@ namespace LanchesMac.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize]
         public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
         {
             var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
