a93540575c8b1e91cbdb329fc75932c51304ef18
Diego Lima de Oliveira <diego.dromer@hotmail.com>
2022-09-20 23:14:21 -0300

    gráficos de apresentação das vendas anuais, mensais e semanais, personalizado para cada tipo de exposição implementado.

--- LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminGraficoController : Controller
     {
         private readonly GraficoVendasService _graficoVendasService;
@@ -20,20 +21,20 @@ namespace LanchesMac.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index(int dias)
+        public IActionResult Index()
         {
             return View();
         }
 
 
         [HttpGet]
-        public IActionResult VendasMensal(int dias)
+        public IActionResult VendasMensal()
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult VendasSemanal(int dias)
+        public IActionResult VendasSemanal()
         {
             return View();
         }
